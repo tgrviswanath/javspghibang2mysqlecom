8db8b3681f9a587579a66916fc078b32aad582c7
tgrviswanath <tgrviswanath@gmail.com>
2019-01-03 22:11:34 +0530

    commit

---
+++ 03myaccount/020 Adding New User Controller/Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.bookstore.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.UserRepository;
+import com.bookstore.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordResetTokenRepository passwordResetTokenRepository;
+	
+	@Override
+	public PasswordResetToken getPasswordResetToken(final String token) {
+		return passwordResetTokenRepository.findByToken(token);
+	}
+	
+	@Override
+	public void createPasswordResetTokenForUser(final User user, final String token) {
+		final PasswordResetToken myToken = new PasswordResetToken(token, user);
+		passwordResetTokenRepository.save(myToken);
+	}
+	
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	public User findByEmail (String email) {
+		return userRepository.findByEmail(email);
+	}
+
+}
---
+++ 10SomeMoreFeatures/081 Bookstore - Add SearchByCategory Logic/Bookstore/src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,13 @@
+package com.bookstore.service;
+
+import java.util.List;
+
+import com.bookstore.domain.Book;
+
+public interface BookService {
+	List<Book> findAll ();
+	
+	Book findOne(Long id);
+	
+	List<Book> findByCategory(String category);
+}
